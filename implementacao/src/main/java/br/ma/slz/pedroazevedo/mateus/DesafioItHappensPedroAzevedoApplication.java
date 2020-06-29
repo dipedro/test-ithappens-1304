9056498341f2131daf540fadba3f90e039c0bea9
Pedro Azevedo <dipedrozd@hotmail.com>
2020-06-28 22:29:12 -0300

    adicionando projeto

---
+++ implementacao/src/main/java/br/ma/slz/pedroazevedo/mateus/DesafioItHappensPedroAzevedoApplication.java
@@ -0,0 +1,13 @@
+package br.ma.slz.pedroazevedo.mateus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItHappensPedroAzevedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItHappensPedroAzevedoApplication.class, args);
+	}
+
+}
---
+++ implementacao/src/test/java/br/ma/slz/pedroazevedo/mateus/DesafioItHappensPedroAzevedoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ma.slz.pedroazevedo.mateus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItHappensPedroAzevedoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
